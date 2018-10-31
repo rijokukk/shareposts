91947b1103b27bd5e07dc2f1dbf709715cbf44db
Riku Kukkaniemi <kukkaniemi.riku@gmail.com>
2018-10-31 18:13:28 +0200

    pushing code to github

---
+++ README.md
@@ -0,0 +1,5 @@
+# SharePosts
+> Simple social network built on the TraversyMVC PHP framework
+
+> Made in Udemy Course https://www.udemy.com/object-oriented-php-mvc/
+
---
+++ app/views/inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <div class="container">
+    
